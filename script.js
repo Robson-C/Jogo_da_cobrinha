bb68cabd9a20f8a5e6eab250840b3dbcbcbdf787
Robson-C <robson_c@live.com>
2021-11-02 10:09:50 -0300

    jogo finalizado, falta estilizar

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function iniciarJogo() {
 	if(direction == "left") snakeX -= box;
 	if(direction == "up") snakeY -= box;
 	if(direction == "down") snakeY += box;
-	snake.pop();
+
 	let newHead = {
 		x: snakeX,
 		y: snakeY
@@ -61,6 +61,23 @@ function iniciarJogo() {
 	if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
 	if(snake[0].y > 15* box && direction == "down") snake[0].y = 0;
 	if(snake[0].y < 0 && direction == "up") snake[0].y = 15 * box;
+	if(snakeX != food.x || snakeY != food.y){
+		snake.pop();
+	
+	}else{
+		food.x = Math.floor(Math.random() * 15 + 1) * box;
+		food.y = Math.floor(Math.random() * 15 + 1) * box;
+	}
+
+
+
+
+	for(i = 1; i < snake.length; i++) {
+		if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+			clearInterval(jogo);
+			alert("Game Over :(");
+		}
+	}
 
 }
 
