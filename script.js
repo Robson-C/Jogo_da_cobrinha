41955db8bc08cce137598b477961abd566cbc2e3
Robson-C <robson_c@live.com>
2021-11-02 09:33:57 -0300

    definir direcoes e impedir que cobrinha suma

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ snake[0] = {
 	x: 8 * box,
 	y: 8 * box
 }
+let direction = "right";
+let jogo = setInterval(iniciarJogo, 100);
+
+
+document.addEventListener('keydown', update);
+
 
 function criarBG() {
 	context.fillStyle = "lightgreen";
@@ -19,6 +25,32 @@ function criarCobrinha(){
 	}	
 }
 
+function iniciarJogo() {
+	criarBG();
+	criarCobrinha();
+	let snakeX = snake[0].x;
+	let snakeY = snake[0].y;
+	if(direction == "right") snakeX += box;
+	if(direction == "left") snakeX -= box;
+	if(direction == "up") snakeY -= box;
+	if(direction == "down") snakeY += box;
+	snake.pop();
+	let newHead = {
+		x: snakeX,
+		y: snakeY
+	}
+	snake.unshift(newHead);
+	
+	if(snake[0].x > 15* box && direction == "right") snake[0].x = 0;
+	if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+	if(snake[0].y > 15* box && direction == "down") snake[0].y = 0;
+	if(snake[0].y < 0 && direction == "up") snake[0].y = 15 * box;
 
-criarBG();
-criarCobrinha();
+}
+
+function update(event) {
+	if(event.keyCode == 37 && direction != "right") direction = "left"; 
+	if(event.keyCode == 38 && direction != "down") direction = "up"; 
+	if(event.keyCode == 39 && direction != "left") direction = "right"; 
+	if(event.keyCode == 40 && direction != "up") direction = "down"; 
+}
